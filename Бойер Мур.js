56b3ebd8b916f74b73cd4c7b46a20c1b8eaf1edd
HELLoWorlD01100 <54909098+HELLoWorlD01100@users.noreply.github.com>
2019-12-21 22:04:07 +0500

    Update Бойер Мур.js

--- Бойер Мур.js
+++ Бойер Мур.js
@@ -18,25 +18,3 @@ for (let i = 0; i < text.length; i ++){
 console.log('Boyer-Moore!')
 console.log(`Line item matching(first symbol!!!): ${result.join(', ')}`);
 console.log(`Number of entries: ${result.length}`)
-
-let startBF = new Date();
-function BruteForce(text, substring){
-	let count = 0;
-    let arr = new Array();
-    for (let i = 0; i < text.length; i++){
-        let count = 0;
-            for (let j = 0; j < substring.length; j++){
-                if (text[i + j] === substring[j])
-                    count++;
-            }
-        if (count === substring.length) arr.push(i)
-        count = 0;
-    }
-    return arr;
-}
-let resBF = BruteForce(text, substring);
-let finishBF = new Date(); 
-console.log(`Brute Force!`)
-console.log(`Runtime: ${finishBF - startBF}`);
-console.log(`Line item matching(first symbol!!!): ${resBF.join(', ')}`);
-console.log(`Number of entries: ${resBF.length}`)
